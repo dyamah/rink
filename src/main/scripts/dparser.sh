7992f82e47059042cb91030afdb4fa6c7c55ac90
dyamah <dyama.h@gmail.com>
2013-01-28 15:51:24 +0900

    remove  App.java backup

--- src/main/scripts/dparser.sh
+++ src/main/scripts/dparser.sh
@@ -2,7 +2,7 @@
 
 M2_REPO=~/.m2/repository
 CLI=${M2_REPO}/commons-cli/commons-cli/1.2/commons-cli-1.2.jar
-RINK=${M2_REPO}/jp/gr/java_conf/dyama/rink/rink/1.0-beta/rink-1.0-beta.jar
+RINK=${M2_REPO}/jp/gr/java_conf/dyama/rink/rink/0.1-beta/rink-0.1-beta.jar
 
 classpath=${CLI}:${RINK}
 pkg=jp.gr.java_conf.dyama.rink
