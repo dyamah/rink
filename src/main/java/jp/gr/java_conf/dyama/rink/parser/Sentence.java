71a67aeb8797a638d5687b8ad494998f2a23871e
dyamah <dyama.h@gmail.com>
2013-02-08 17:07:00 +0900

    revise API document

--- src/main/java/jp/gr/java_conf/dyama/rink/parser/Sentence.java
+++ src/main/java/jp/gr/java_conf/dyama/rink/parser/Sentence.java
@@ -2,22 +2,23 @@ package jp.gr.java_conf.dyama.rink.parser;
 public interface Sentence {
 
     /**
-     * get the number of words
+     * Returns the number of words
      * @return number of words
      */
     public int size();
 
     /**
-     * get the i-th word
-     * @param i index. throw IllegalArgumentException if i is negative or greater than {@link #size()} - 1.
-     * @return word
+     * Returns the i-th word
+     * @param i the index.
+     * @return word corresponding to the index.
+     * @throws IllegalArgumentException if i is negative or greater than {@link #size()} - 1.
      */
     public Word getWord(int i);
 
 
     /**
-     * get the string of the sentence.
-     * @return string of the sentence
+     * Returns the string of the sentence.
+     * @return the string of the sentence
      */
     public String getString();
 
