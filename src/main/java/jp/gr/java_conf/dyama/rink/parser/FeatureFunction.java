1c4d01402a9efda913ee356a9135e67ab1c84827
dyamah <dyama.h@gmail.com>
2013-02-08 16:48:06 +0900

    revise API doc.

--- src/main/java/jp/gr/java_conf/dyama/rink/parser/FeatureFunction.java
+++ src/main/java/jp/gr/java_conf/dyama/rink/parser/FeatureFunction.java
@@ -5,8 +5,9 @@ import java.io.Serializable;
 public interface FeatureFunction extends Serializable {
 
     /**
-     * apply this feature function to the target sample. (= extract features from the sample, and store them to the feature buffer of the sample.)
-     * @param sample target sample. throw IllegalArgumentException if the sample is null.
+     * Applies this feature function to the target sample. (= extract features from the sample, and store them to the feature buffer of the sample.)
+     * @param sample the target sample.
+     * @throws IllegalArgumentException if the sample is null.
      */
     public void apply(Sample sample);
 
