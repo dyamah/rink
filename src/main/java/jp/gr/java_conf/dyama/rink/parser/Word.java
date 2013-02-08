6f38f7648ff8da847d694d713e7bab9597a08734
dyamah <dyama.h@gmail.com>
2013-02-08 17:16:48 +0900

    revise API document

--- src/main/java/jp/gr/java_conf/dyama/rink/parser/Word.java
+++ src/main/java/jp/gr/java_conf/dyama/rink/parser/Word.java
@@ -6,45 +6,45 @@ public interface Word extends Range {
 
     /**
      * Returns the beginning position of the word.
-     * @return the beginning position of the word
+     * @return the beginning position of the word.
      */
     @Override
     public int getBegin();
 
     /**
      * Returns the ending position of the word.
-     * @return the ending position of the word
+     * @return the ending position of the word.
      */
     @Override
     public int getEnd();
 
     /**
      * Returns the length of the word ( = the number of characters )
-     * @return the length of the word
+     * @return the length of the word.
      */
     public int length();
 
     /**
      * Returns the word's ID.
-     * @return word's ID
+     * @return word's ID.
      */
     public int getID();
 
 
     /**
-     * Returns the surface string of the word
+     * Returns the surface string of the word.
      * @return the surface string of the word.
      */
     public String getSurface();
 
     /**
-     * Returns the base form of the word
-     * @return base form string
+     * Returns the base form of the word.
+     * @return base form string.
      */
     public String getBaseForm();
 
     /**
-     * Returns the part-of-speech tag
+     * Returns the part-of-speech tag.
      * @return the part-of-speech tag.
      */
     public TagSet.PartOfSpeech getPOS();
