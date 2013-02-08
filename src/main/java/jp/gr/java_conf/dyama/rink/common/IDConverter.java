957075f8749971cacf8ad7d347638ae50507a217
dyamah <dyama.h@gmail.com>
2013-02-08 13:54:16 +0900

    revise Java doc

--- src/main/java/jp/gr/java_conf/dyama/rink/common/IDConverter.java
+++ src/main/java/jp/gr/java_conf/dyama/rink/common/IDConverter.java
@@ -1,25 +1,25 @@
 package jp.gr.java_conf.dyama.rink.common;
 
 public interface IDConverter {
-    /** reserved ID for undefined string and null. */
+    /** the reserved ID for undefined string and null. */
     public static final int UNDEFINED      = 0;
 
-    /** reserved ID for BOS */
+    /** the reserved ID for BOS */
     public static final int BOS            = 1;
 
-    /** reserved ID for EOS */
+    /** the reserved ID for EOS */
     public static final int EOS            = 2;
 
-    /** reserved ID for EOS */
+    /** the reserved ID for EOS */
     public static final int NONE           = 3;
 
-    /** starting ID for normal strings */
+    /** the starting ID for normal strings */
     static final int START                 = 10;
 
     /**
-     * convert a string into positive unique ID
-     * @param string  target string.
-     * @return unique ID. return 0 if string is null.
+     * Converts a string into positive unique ID
+     * @param string  the target string.
+     * @return ID. return 0 if string is null.
      */
     public int convert(String string);
 }
