49e387c4cd902d7cd986540ffb239c149c83690d
dyamah <dyama.h@gmail.com>
2013-02-08 17:21:45 +0900

    revise API doc.

--- src/main/java/jp/gr/java_conf/dyama/rink/parser/core/ActionEstimator.java
+++ src/main/java/jp/gr/java_conf/dyama/rink/parser/core/ActionEstimator.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 public interface ActionEstimator extends Serializable {
 
     /**
-     * estimate the best parsing action at the current position in the target sample.
+     * Estimates the best parsing action at the current position in the target sample.
      * @param sample the target sample.
      * @return action the best parsing action.
      * @throw IllegalArgumentException if the sample is null.
