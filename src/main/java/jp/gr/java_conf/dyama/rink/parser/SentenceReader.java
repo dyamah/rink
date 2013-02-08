de7f1d8aa0a525ab4476332154a7bdefe2c760d6
dyamah <dyama.h@gmail.com>
2013-02-08 17:13:09 +0900

    revise API document

--- src/main/java/jp/gr/java_conf/dyama/rink/parser/SentenceReader.java
+++ src/main/java/jp/gr/java_conf/dyama/rink/parser/SentenceReader.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 public interface SentenceReader {
 
     /**
-     * check whether the reader can read any sentences or not.
+     * Checks whether the reader can read any sentences or not.
      * @return true if the reader can read some sentences, otherwise false
      */
     boolean hasAnySentences() throws IOException ;
 
     /**
-     * read one sentence.
-     * @param sentence the object to save the reading sentence.
+     * Reads one sentence.
+     * @param sentence the object to save the reading sentence information.
      * @return true if the reader can read a sentence, otherwise false.
      * @throws IllegalArgumentException if the sentence is null.
      * @throws IOException if it fails to read.
@@ -21,7 +21,7 @@ public interface SentenceReader {
 
 
     /**
-     * close the reader.
+     * Closes the reader.
      * @throws IOException if it fails to close.
      */
     public void close() throws IOException;
