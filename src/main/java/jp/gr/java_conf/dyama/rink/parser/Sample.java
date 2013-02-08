ff8352a1bd0dca89cbd17b967af4022967fcfced
dyamah <dyama.h@gmail.com>
2013-02-08 17:04:26 +0900

    revise API document

--- src/main/java/jp/gr/java_conf/dyama/rink/parser/Sample.java
+++ src/main/java/jp/gr/java_conf/dyama/rink/parser/Sample.java
@@ -3,7 +3,7 @@ package jp.gr.java_conf.dyama.rink.parser;
 import java.io.IOException;
 
 /**
- * Parsing Sample
+ * Sample for parsing.
  * @author Hiroyasu Yamada
  *
  */
@@ -11,31 +11,31 @@ public interface Sample {
 
 
     /**
-     * parse one step  at the current position
+     * Parses one step  at the current position
      * @return true if the sample can be parsed. return false if the sample can not be parsed any more.
      */
     public boolean parseOneStep();
 
     /**
-     * prepare to re-parse the current sentence
+     * Prepares to re-parse the current sentence
      */
     public void reparse();
 
     /**
-     * prepare to re-parse the current sentence, and set the start position
-     * @param start
+     * Prepares to re-parse the current sentence, and set the start position.
+     * @param start the start position.
      */
     public void reparse(int start);
 
     /**
-     * get the sentence
+     * Returns the sentence.
      * @return sentence
      */
     public Sentence getSentence();
 
     /**
-     * read a new sentence and setup.
-     * @return true if the reader can read  a new sentence and setup. return false if there is no sentence.
+     * Reads a new sentence and sets up .
+     * @return true if the reader can read a new sentence and setup. return false if there is no readable sentence.
      * @throws IOException if the reader fail to read a sentence from input stream.
      */
     public boolean read() throws IOException;
